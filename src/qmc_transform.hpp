a06520c93cb26a94aaf2bb6e5bf2c9f030d30258
Stephen Jones <sjones@mpp.mpg.de>
2018-03-12 16:26:02 +0100

    add BorderedKorobov

--- src/qmc_transform.hpp
+++ src/qmc_transform.hpp
@@ -19,4 +19,27 @@ namespace integrators
 			}
 		};
 	};
+    
+    template <typename D, typename U>
+    struct BorderedKorobov3 {
+        D border = 1e-13;
+#ifdef __CUDACC__
+        __host__ __device__
+#endif
+        void operator()(D* x, D& wgt, const U dim) const
+        {
+            // Korobov r = 3
+            for (U sDim = 0; sDim < dim; sDim++)
+            {
+                wgt *= x[sDim] * x[sDim] * x[sDim] * 140.*(1. - x[sDim])*(1. - x[sDim])*(1. - x[sDim]);
+                x[sDim] = x[sDim] * x[sDim] * x[sDim] * x[sDim] * (35. + x[sDim] * (-84. + x[sDim] * (70. + x[sDim] * (-20.))));
+                // loss of precision can cause x > 1., must keep in x \elem [0,1]
+                if (x[sDim] > 1.)
+                    x[sDim] = 1.-border;
+                if (x[sDim] < border)
+                    x[sDim] = border;
+            }
+        };
+    };
+
 };
