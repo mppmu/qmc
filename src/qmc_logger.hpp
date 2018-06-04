0e3a58c6a3f08437b1c646c63dab8e939a934161
Stephen Jones <sjones@mpp.mpg.de>
2018-06-04 19:17:43 +0200

    [logger] shadow reference_wrapper constructor (nvcc workaround)

--- src/qmc_logger.hpp
+++ src/qmc_logger.hpp
@@ -7,10 +7,10 @@ namespace integrators
 {
     struct Logger : public std::reference_wrapper<std::ostream>
     {
-        using std::reference_wrapper<std::ostream>::reference_wrapper;
+        //        using std::reference_wrapper<std::ostream>::reference_wrapper;
 
-        bool display_timing = true;
-        std::chrono::steady_clock::time_point init_time = std::chrono::steady_clock::now(); // time logger was initialised
+        bool display_timing;
+        std::chrono::steady_clock::time_point reference_time; // time logger was initialised
 
         template<typename T> std::ostream& operator<<(T arg) const
         {
@@ -18,10 +18,14 @@ namespace integrators
             if( display_timing )
             {
                 std::chrono::steady_clock::time_point now_time = std::chrono::steady_clock::now();
-                time_string = "[" + std::to_string(std::chrono::duration_cast<std::chrono::microseconds>(now_time - init_time).count()) + " ms] ";
+                time_string = "[" + std::to_string(std::chrono::duration_cast<std::chrono::microseconds>(now_time - reference_time).count()) + " ms] ";
             }
             return this->get() << time_string << arg;
         }
         std::ostream& operator<<(std::ostream& (*arg)(std::ostream&)) const { return this->get() << arg; }
+
+        Logger(std::ostream& stream) : reference_wrapper(std::ref(stream)), display_timing(true), reference_time(std::chrono::steady_clock::now()) {}
+
     };
 };
+
