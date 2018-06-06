d3be37f94260751f33d890fb169722a8fb168be5
Stephen Jones <sjones@mpp.mpg.de>
2018-06-06 19:23:17 +0200

    [transforms] add tent

--- src/qmc_transform.hpp
+++ src/qmc_transform.hpp
@@ -156,6 +156,23 @@ namespace integrators
             }
         };
 
+        template <typename D, typename U>
+        struct Tent
+        {
+#ifdef __CUDACC__
+            __host__ __device__
+#endif
+            void operator()(D* x, D& wgt, const U dim) const
+            {
+                for (U s = 0; s < dim; s++)
+                {
+                    x[s] = D(1) - fabs(D(2)*x[s]-D(1)) ;
+                    // loss of precision can cause x > 1, must keep in x \elem [0,1]
+                    if (x[s] > D(1)) x[s] = D(1);
+                }
+            }
+        };
+
         template<typename D, typename U>
         struct Trivial
         {
