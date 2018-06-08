cd6f630ae28de6c67c0f3be3fa4fee24be1e8fdf
Stephen Jones <sjones@mpp.mpg.de>
2018-06-08 10:58:09 +0200

    [transforms] fix KorobovCoefficient when call with int type

--- src/qmc_transform.hpp
+++ src/qmc_transform.hpp
@@ -81,13 +81,13 @@ namespace integrators
             template<typename D, typename U, U k, U a, U b, typename = void>
             struct KorobovCoefficient
             {
-                constexpr static D value = D(-1)*(b-k+D(1))/k*(a+k)/(a+k+D(1))*KorobovCoefficient<D,U,k-1,a,b>::value;
+                constexpr static D value = (D(-1)*(D(b)-D(k)+D(1))*D(KorobovCoefficient<D,U,k-1,a,b>::value)*(D(a)+D(k)))/(D(k)*(D(a)+D(k)+D(1)));
             };
 
             template<typename D, typename U, U k, U a, U b>
             struct KorobovCoefficient<D, U, k, a, b, typename std::enable_if<k == 0>::type>
             {
-                constexpr static D value = (D(2)*b+D(1))/(a+D(1))*Binomial<U,2*b,b>::value;
+                constexpr static D value = ((D(2)*D(b)+D(1))*D(Binomial<U,2*b,b>::value))/(D(a)+D(1));
             };
 
             template<typename D, typename U, U k, U a, U b, typename = void>
