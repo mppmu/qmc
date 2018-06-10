84d91ee2c02e84209f6b3aef0c1bb154f590268d
Stephen Jones <sjones@mpp.mpg.de>
2018-06-10 23:22:38 +0200

    [refactor] fix make_singleheader.py

--- make_singleheader.py
+++ make_singleheader.py
@@ -5,10 +5,8 @@
 #####################################################
 
 import os
+import datetime
 from subprocess import check_output as shell_call, CalledProcessError, STDOUT
-
-with open(os.path.join('src','qmc.hpp'),'r') as f:
-    main_header = f.readlines()
 
 # get git commit id and write into header file
 try:
@@ -18,21 +16,51 @@ except CalledProcessError:
 # in python3, the return type of `shell_call` may be `bytes` but we need `str`
 if not isinstance(git_id, str):
     git_id = git_id.decode()
-main_header.insert(0,'// qmc single header, generated from commit "' + git_id + '"\n\n')
 
-# insert qmc headers into main header
-for idx,line in enumerate(main_header):
-    if not line.startswith('#include "qmc_'):
-        continue
+def flatten(input_list):
+    new_list = []
+    for item in input_list:
+        if type(item) == type([]):
+            new_list.extend(flatten(item))
+        else:
+            new_list.append(item)
+    return new_list
 
-    assert line.endswith('"\n')
-    include_filename = line[len('#include "'):-len('"\n')]
+def parse_header(header_filename, header_path, known_headers=set()):
+    with open(os.path.join(header_path,header_filename), 'r') as f:
+        header = f.readlines()
 
-    with open(os.path.join('src',include_filename),'r') as include_header:
-        main_header[idx] = '/*\n * begin included header "' + include_filename + '"\n */\n' \
-                         + include_header.read() + '\n' \
-                         + '/*\n * end included header "' + include_filename + '"\n */\n\n'
+    for idx, line in enumerate(header):
+        if not line.startswith('#include "'):
+            continue
+
+        assert line.endswith('"\n')
+        include_path, include_filename = os.path.split(line[len('#include "'):-len('"\n')])
+        if os.path.abspath(os.path.join(header_path,include_path,include_filename)) not in known_headers:
+            print(os.path.abspath(os.path.join(header_path,include_path,include_filename)))
+            known_headers.add(os.path.abspath(os.path.join(header_path,include_path,include_filename)))
+            include = parse_header(include_filename, os.path.join(header_path, include_path))
+            header[idx] = include
+        else:
+            header[idx] = '// (Included Above): ' + line
+
+    return header
+
+main_header = parse_header('qmc.hpp','src')
+# Add top banner
+main_header.insert(0,
+"""/*
+ * Qmc Single Header
+ * Commit: """ + git_id + """
+ * Generated: """ +  datetime.datetime.now().strftime('%d-%m-%Y %X') + """
+ *
+ * ----------------------------------------------------------
+ * This file has been merged from multiple headers. 
+ * Please don't edit it directly
+ * ----------------------------------------------------------
+ */
+""")
 
 # output single header file
 with open('qmc.hpp','w') as out:
-    out.writelines(main_header)
+    out.writelines(flatten(main_header))
