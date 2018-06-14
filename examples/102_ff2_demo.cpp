ce46cd71a9c893d6ed22c4d496e7e9de13abcc63
Stephen Jones <sjones@mpp.mpg.de>
2018-06-14 11:44:00 +0200

    [examples] add ff2_demo

---
+++ examples/102_ff2_demo.cpp
@@ -0,0 +1,75 @@
+/*
+ * Compile without GPU support:
+ *   c++ -std=c++11 -pthread -I../src 102_ff2_demo.cpp -o 102_ff2_demo.out
+ * Compile with GPU support:
+ *   nvcc -std=c++11 -x cu -I../src 102_ff2_demo.cpp -o 102_ff2_demo.out
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <map>
+
+#include "qmc.hpp"
+
+struct formfactor2L_t {
+#ifdef __CUDACC__
+    __host__ __device__
+#endif
+    double operator()(const double arg[]) const
+    {
+
+        double x0  = arg[0];
+        double x1  = (1.-x0)*arg[1];
+        double x2  = (1.-x0-x1)*arg[2];
+        double x3  = (1.-x0-x1-x2)*arg[3];
+        double x4  = (1.-x0-x1-x2-x3)*arg[4];
+        double x5  = (1.-x0-x1-x2-x3-x4);
+
+        double wgt =
+        (1.-x0)*
+        (1.-x0-x1)*
+        (1.-x0-x1-x2)*
+        (1.-x0-x1-x2-x3);
+
+        if(wgt <= 0) return 0;
+
+        double u=x2*(x3+x4)+x1*(x2+x3+x4)+(x2+x3+x4)*x5+x0*(x1+x3+x4+x5);
+        double f=x1*x2*x4+x0*x2*(x1+x3+x4)+x0*(x2+x3)*x5;
+        double n=x0*x1*x2*x3;
+        double d = f*f*u*u;
+
+        return wgt*n/d;
+    }
+} formfactor2L;
+
+int main() {
+
+    using D = double;
+    using U = unsigned long long int;
+
+    integrators::Qmc<D,D> integrator;
+    integrator.minm = 20;
+    integrator.maxeval = 1; // do not iterate
+
+    // Append large generating vectors to default generating vectors
+    std::map<U,std::vector<U>> large_vecs = integrators::generatingvectors::cbcpt_cfftw1_6<U>();
+    integrator.generatingvectors.insert(large_vecs.begin(),large_vecs.end());
+
+    integrators::transforms::Korobov<double,unsigned long long int,5> integral_transform;
+
+    std::cout << "# n m Re[I] Im[I] Re[Abs. Err.] Im[Abs. Err.]" << std::endl;
+    std::cout << std::setprecision(16);
+
+    for(const auto& generating_vector : integrator.generatingvectors)
+    {
+        integrator.minn = generating_vector.first;
+        integrators::result<double> result = integrator.integrate(formfactor2L,5,integral_transform);
+
+        std::cout
+        << result.n
+        << " " << result.m
+        << " " << result.integral
+        << " " << result.error
+        << std::endl;
+    }
+}
--- examples/README.md
+++ examples/README.md
@@ -122,7 +122,12 @@ The functor's call operator decides whether to call the host or device version o
  
  ### 101_ff4_demo
  
- The finite 4-loop form factor of Eq(7.1) [arXiv:1510.06758](https://arxiv.org/abs/1510.06758). Here, rather than applying sector decomposition (which generates O(20k) sectors) we try to integrate the function directly. The first few lines map the domain of integration from the simplex to the hypercube. The example depends on 11 Feynman parameters (integration variables) and we find that (due to the high dimension?) the variance of the function is extremely large particualrly when the Korobov transforms are applied. Note that although the input function is integrable it is not finite everywhere, in particular it blows up when some of the parameters tend to zero.
+ The finite 4-loop form factor of Eq(7.1) [arXiv:1510.06758](https://arxiv.org/abs/1510.06758). Here, rather than applying sector decomposition (which generates O(20k) sectors) we try to integrate the function directly. The first few lines map the domain of integration from the simplex to the hypercube. The example depends on 11 Feynman parameters (integration variables) and we find that (due to the high dimension?) the variance of the function is extremely large particualrly when the Korobov transforms are applied. Note that although the input function is integrable it is not finite everywhere, in particular it is singular when some of the parameters tend to zero.
  
  The analytic result evalutes to: `3.1807380843134699650...`
  
+ ### 102_ff2_demo
+ 
+ A finite 2-loop form factor without sector decompositon, called `INT[“B2diminc4”, 6, 63, 10, 0, {2, 2, 2, 2, 1, 1, 0}]` in [arXiv:1510.06758](https://arxiv.org/abs/1510.06758) (see references therein for the much earlier original calculations). Note that although the input function is integrable it is not finite everywhere, in particular it singular when some of the parameters tend to zero.
+ 
+ The analytic result evalutes to: `0.27621049702196548441...`
