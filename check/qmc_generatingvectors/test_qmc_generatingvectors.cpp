7b05ab63b63ab57caf954f7e737562aa4a357d31
Stephen Jones <sjones@mpp.mpg.de>
2018-09-13 12:40:10 +0200

    [check] add generatingvectors test case

---
+++ check/qmc_generatingvectors/test_qmc_generatingvectors.cpp
@@ -0,0 +1,41 @@
+#include "catch.hpp"
+#include "qmc.hpp"
+
+TEST_CASE( "cbcpt_cfftw1_6", "[generatingvectors]")
+{
+    using U = unsigned long long int;
+
+    std::map<U,std::vector<U>> gvec = integrators::generatingvectors::cbcpt_cfftw1_6<U>();
+
+    SECTION("Length")
+    {
+        REQUIRE( gvec.lower_bound(1)->second.size() == 6);
+    };
+
+};
+
+TEST_CASE( "cbcpt_dn1_100", "[generatingvectors]")
+{
+    using U = unsigned long long int;
+
+    std::map<U,std::vector<U>> gvec = integrators::generatingvectors::cbcpt_dn1_100<U>();
+
+    SECTION("Length")
+    {
+        REQUIRE( gvec.lower_bound(1)->second.size() == 100);
+    };
+
+};
+
+TEST_CASE( "cbcpt_dn2_6", "[generatingvectors]")
+{
+    using U = unsigned long long int;
+
+    std::map<U,std::vector<U>> gvec = integrators::generatingvectors::cbcpt_dn2_6<U>();
+
+    SECTION("Length")
+    {
+        REQUIRE( gvec.lower_bound(1)->second.size() == 6);
+    };
+
+};
