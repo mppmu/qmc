f716f07b4d39881e59f1f39b5af0a7e8c41837c8
Stephen Jones <sjones@mpp.mpg.de>
2018-10-08 13:33:51 +0200

    [examples] fix gpu compilation

--- examples/100_hh_sector_demo.cpp
+++ examples/100_hh_sector_demo.cpp
@@ -2,7 +2,7 @@
  * Compile without GPU support:
  *   c++ -std=c++11 -pthread -I../src 100_hh_sector_demo.cpp -o 100_hh_sector_demo.out -lgsl -lgslcblas
  * Compile with GPU support:
- *   nvcc -std=c++11 -x cu -I../src 100_hh_sector_demo.cpp -o 100_hh_sector_demo -lgsl -lgslcblas
+ *   nvcc -std=c++11 -x cu -I../src 100_hh_sector_demo.cpp -o 100_hh_sector_demo.out -lgsl -lgslcblas
  */
 
 #include <iostream>
