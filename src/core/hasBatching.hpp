1630c6d6643f7c41577b3310ad67d8b47d02748f
Alexander Smirnov <asmirnov80@gmail.com>
2021-08-20 16:24:45 +0300

    now the return type of overloaded operator should be void

--- src/core/hasBatching.hpp
+++ src/core/hasBatching.hpp
@@ -5,8 +5,8 @@ template <typename T, typename DP, typename RP, typename U> static constexpr boo
     return false;
 }
 
-template <typename T, typename DP, typename RP, typename U> static constexpr bool hasBatching(int, decltype((std::declval<T>().operator()(DP(), RP(), U()))) = 0) {
-    return true;
+template <typename T, typename DP, typename RP, typename U> static constexpr bool hasBatching(int, decltype(std::is_void<decltype(std::declval<T>().operator()(DP(), RP(), U()))>::value) result = false) {
+    return std::is_void<decltype(std::declval<T>().operator()(DP(), RP(), U()))>::value;
 }
 
 #endif
